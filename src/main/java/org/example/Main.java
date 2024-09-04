7680eb34488d037b0608009e35fefedffc7b058f
yoonseo1265 <yoonseo1265@gmail.com>
2024-09-04 15:39:38 +0900

    koko

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,13 @@
 package org.example;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Parrot p = new Parrot();
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        Parrot p = context.getBean(Parrot.class);
+        System.out.println(p.getName());
     }
 }
