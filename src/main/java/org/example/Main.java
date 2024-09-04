cdb736948ceb52a729afe5e0da7d8e4131505598
yoonseo1265 <yoonseo1265@gmail.com>
2024-09-04 17:01:57 +0900

    String integer

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,5 +9,9 @@ public class Main {
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
         Parrot p = context.getBean(Parrot.class);
         System.out.println(p.getName());
+        String s = context.getBean(String.class);
+        System.out.println(s);
+        Integer n = context.getBean(Integer.class);
+        System.out.println(n);
     }
 }
